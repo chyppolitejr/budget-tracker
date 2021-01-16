6716c0766be2d173b94583db3fb5fab8ebde0c3e
chyppoli <claude.hyppolite@gmail.com>
2021-01-15 21:28:30 -0500

    add useUnifiedTopology

--- server.js
+++ server.js
@@ -1,25 +1,28 @@
-
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
+const path = require("path");
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
 app.use(logger("dev"));
-
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.static("public"));
 
-app.use(express.static("public"));
-
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budgetTracker", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/budgetTracker",
+  {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  }
+);
 
 // routes
 app.use(require("./routes/api.js"));
