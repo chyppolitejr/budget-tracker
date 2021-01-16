c5cbf3669d09236657856218a484eec429798195
chyppoli <claude.hyppolite@gmail.com>
2021-01-15 21:08:50 -0500

    replace entire contents of server.js

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
@@ -8,34 +9,20 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(logger("dev"));
-// app.use(express.static(path.join(__dirname, "public")));
+
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/budgetTracker",
-  {
-    useNewUrlParser: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-  }
-);
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budgetTracker", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
 
 // routes
 app.use(require("./routes/api.js"));
-
-const connection = mongoose.connection;
-
-connection.on("connected", () => {
-  console.log("Mongoose successfully connected!");
-});
-
-connection.on("error", (err) => {
-  console.log("Mongoose connection error: ", err);
-});
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
