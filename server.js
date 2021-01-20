0f236d5872387495c1f20a9096ec4db6db010089
chyppoli <claude.hyppolite@gmail.com>
2021-01-20 08:03:27 -0500

    update mongoose.connect. add useCreateIndex

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
-// const path = require("path");
+const path = require("path");
 
 const PORT = process.env.PORT || 3000;
 
@@ -15,17 +15,23 @@ app.use(express.json());
 // app.use(express.static(path.join(__dirname, "public")));
 app.use(express.static("public"));
 
+
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost/budgetTracker",
   {
     useNewUrlParser: true,
-    useFindAndModify: false,
     useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
   }
 );
 
 // routes
 app.use(require("./routes/api.js"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "./public/index.html"));
+});
 
 app.listen(PORT, () => {  
   console.log(`App running on port ${PORT}!`);
